9da434221191cfdb5a2ed4776af5c0ee346a883f
AbiR7171 <abiremtiag@gmail.com>
2023-11-03 15:47:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000 ;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", async(req,res)=>{
+     
+          res.send("Eduman server is ready to gooo")
+})
+
+app.listen(port, ()=>{
+          console.log(`Eduman is running on ${port}`);
+})
